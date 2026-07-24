231b7d80e589053d2d2427cacd03f29e3e05edc5
Nayaka Harshitha <harshitharoyal28@gmail.com>
2026-07-24 21:12:22 +0530

    Member 2: Database setup and project configuration

---
+++ snowflake_config.py
@@ -0,0 +1,15 @@
+import snowflake.connector
+
+conn = snowflake.connector.connect(
+    user="HARSHITHA",
+    password="Harshitharoyal@1",
+    account="WPCUUVA-AH54929",
+    warehouse="COMPUTE_WH",
+    database="IOT_DB",
+    schema="PUBLIC",
+    role="ACCOUNTADMIN"
+)
+
+print("✅ Connected to Snowflake Successfully!")
+
+conn.close()
